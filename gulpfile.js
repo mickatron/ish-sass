9a03f168cb182b2d66899b3aaa8529b48513e4ef
Michael Hargreaves <code@digitalfeast.com.au>
2017-04-05 20:52:06 +1000

    update license and gulp process to include release

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 'use strict';
 const gulp			= require('gulp');
 const sassdoc 		= require('sassdoc');
+const bump 			= require('gulp-bump');
+const gutil 		= require('gulp-util');
+const git 			= require('gulp-git');
+const runSequence 	= require('run-sequence');
 
 const _docFolder = './docs';
 const _srcFiles = './src/**/*.scss';
@@ -27,3 +31,69 @@ gulp.task('watch', function() {
 gulp.task('default', ['clean'], function() {
   gulp.start('sassdoc');
 });
+
+// $ gulp release --type (major || minor || patch)
+gulp.task('release', function() {
+runSequence(
+    //'merge-changes',
+	//'default',
+    'bump-version',
+    'commit-changes',
+    'push-changes',
+    'create-new-tag',
+    //'github-release',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
+
+gulp.task('bump-version', function () {
+	var type = gulpUtil.env.type || "patch";
+	console.log('Release patch type: ', type);
+// We hardcode the version change type to 'patch' but it may be a good idea to
+// use minimist (https://www.npmjs.com/package/minimist) to determine with a
+// command argument whether you are doing a 'major', 'minor' or a 'patch' change.
+  return gulp.src(['./package.json'])
+    .pipe(bump({type: type}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+    //.pipe(git.add())
+    .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+    git.push('origin', 'master', {args: '--tags'}, cb);
+  });
+
+  function getPackageJsonVersion () {
+    // We parse the json file instead of using require because require caches
+    // multiple calls so the version number won't be updated
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
+
+/*gulp.task('merge-changes', function () {
+  return 
+	.pipe(git.merge('staging', 
+		function (err) { 
+			if (err) throw err;
+		})
+	)
+});*/
